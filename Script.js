2d34a24c37d485fbe5fcaf538271b775fad9f782
Dontosquare <153940558+Dontosquare@users.noreply.github.com>
2025-05-11 13:06:37 +0200

    Javascript loops parameters test

--- Script.js
+++ Script.js
@@ -8,3 +8,14 @@ hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
 })
+
+let params = new URLSearchParams('product_id=produkter&quantity=42');
+
+for(let param of params){
+    console.log(param)
+}
+
+for(let [product_id, produkter] of params){
+    console.log(product_id)
+    console.log(produkter)
+}
