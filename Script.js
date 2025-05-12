b274f69cb687983f10d75ddaeb107474f204b100
Dontosquare <153940558+Dontosquare@users.noreply.github.com>
2025-05-12 08:49:41 +0200

    Burger menu færdig

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
 
-
+// Burger menu javascript //
 const hamMenu = document.querySelector('.ham-menu');
 
 const offScreenMenu = document.querySelector('.off-screen-menu');
@@ -9,6 +9,8 @@ hamMenu.addEventListener('click', () => {
     offScreenMenu.classList.toggle('active');
 })
 
+
+/*
 let params = new URLSearchParams('product_id=produkter&quantity=42');
 
 for(let param of params){
@@ -18,4 +20,19 @@ for(let param of params){
 for(let [product_id, produkter] of params){
     console.log(product_id)
     console.log(produkter)
+}
+
+*/
+
+document.getElementById('forside').onclick = function () {
+    location.href = 'index.html';
+}
+document.getElementById('produkter').onclick = function () {
+    location.href = 'produkter.html';
+}
+document.getElementById('sek').onclick = function () {
+    location.href = 'bygenkurv.html'
+}
+document.getElementById('kontakt').onclick = function () {
+    location.href = 'kontakt.html'
 }
